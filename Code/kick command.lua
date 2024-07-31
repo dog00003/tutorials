e7ed4c722502a1e59e2920ea97c0099d3608bdc4
dog00003 <108981379+dog00003@users.noreply.github.com>
2024-07-31 20:51:49 +0100

    Create kick command.lua

---
+++ Code/kick command.lua
@@ -0,0 +1,19 @@
+local admins = {
+	{user = "dog00003", rank = 1},
+	{user = "otheruser", rank = 3},
+}
+
+game.Players.PlayerAdded:Connect(function(plr)
+	plr.Chatted:Connect(function(message)
+		for i,v in pairs(admins) do
+			if v.user == plr.Name then
+				print("user is an admin")
+				if message == "/Kick all" and v.rank == 1 then
+					for i,v in pairs(game.Players:GetChildren()) do
+						v:kick("You have been kicked by an admin")
+					end
+				end
+			end
+		end
+	end)
+end)
