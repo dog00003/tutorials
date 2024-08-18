1bc2225683a6d130010fe189a505dca09a853dca
oOAlmaxOo <99538458+oOAlmaxOoYT@users.noreply.github.com>
2024-08-18 22:52:42 +0200

    Update kick command.lua
    The comparison of ranks has been changed from == to >=. This modification makes sure that any admin can use the "/Kick all" command if their rank is at least as high as the threshold.

--- Code/kick command.lua
+++ Code/kick command.lua
@@ -8,7 +8,7 @@ game.Players.PlayerAdded:Connect(function(plr)
 		for i,v in pairs(admins) do
 			if v.user == plr.Name then
 				print("user is an admin")
-				if message == "/Kick all" and v.rank == 1 then
+				if message == "/Kick all" and v.rank >= 1 then
 					for i,v in pairs(game.Players:GetChildren()) do
 						v:kick("You have been kicked by an admin")
 					end
